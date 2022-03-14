087909235507012e7229a82f2e1d254cb5cbfca3
healingfields <dihratene@gmail.com>
2022-03-14 17:22:22 +0100

    added search filtering to my api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,12 +7,15 @@ from .permissions import UpdateOwnProfile
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = [UpdateOwnProfile]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ('name', 'email',)
 
 
 
