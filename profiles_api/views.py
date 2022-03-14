2f9359f39efcce6bdc03156c90ca5a225ee0d5db
healingfields <dihratene@gmail.com>
2022-03-14 18:48:56 +0100

    added permission to my feed viweset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .serializers import UserProfileSerializer, ProfileFeedItemSerializer
 from .models import UserProfile, ProfileFeedItem
-from .permissions import UpdateOwnProfile
+from .permissions import UpdateOwnProfile, UpdateOwnStatus
 
 from rest_framework import viewsets
 from rest_framework import status
@@ -10,6 +10,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
@@ -26,6 +27,7 @@ class ProfileFeedViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileFeedItemSerializer
     queryset = ProfileFeedItem.objects.all()
     authentication_classes = [TokenAuthentication]
+    permission_classes = [UpdateOwnStatus, IsAuthenticatedOrReadOnly ]
 
     def perform_create(self, serializer):
         serializer.save(user_profile=self.request.user)
