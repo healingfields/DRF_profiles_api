66051e9a4c01ce8eb2cb54116c585980ebddbbb0
healingfields <dihratene@gmail.com>
2022-03-14 17:33:23 +0100

    added login to my endpoints using tokens

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,7 +8,8 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
@@ -16,6 +17,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = [UpdateOwnProfile]
     filter_backends = [filters.SearchFilter]
     search_fields = ('name', 'email',)
+
+class UserLoginView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 
