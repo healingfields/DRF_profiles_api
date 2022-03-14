1ead97fbe54cf2ad952182ba905642328765a51f
healingfields <dihratene@gmail.com>
2022-03-14 17:19:12 +0100

    added profileUpdate Permission

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 
 from .serializers import UserProfileSerializer
 from .models import UserProfile
+from .permissions import UpdateOwnProfile
 
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [UpdateOwnProfile]
 
 
 
