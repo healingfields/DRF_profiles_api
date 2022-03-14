816854477ac63f755dc4d243ce4426c71cf810ce
healingfields <dihratene@gmail.com>
2022-03-14 17:31:55 +0100

    added login to my endpoints

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,8 +4,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('profile', views.UserProfileViewSet)
+router.register('profile', views.UserProfileViewSet),
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login/', views.UserLoginView.as_view()),
 ]
